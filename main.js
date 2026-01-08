d6689650e914fc60c6b8273f63257f2d3b96fb4d
diriba <dirocode24@gmail.com>
2026-01-08 10:58:49 -0800

    simple caclutator

--- main.js
+++ main.js
@@ -3,32 +3,32 @@ const numInput1 = document.getElementById("num1")
 const numInput2 = document.getElementById("num2")
 const resultDispaly = document.getElementById("result");
 
-function getNumber(){
-    const num1 = Number(numInput1.value);
-    const num2 = Number(numInput2.value);
-    return {num1,num2};
-}
+     
 const addBtn = ()=>{
-   const {num1,num2}=  getNumber()
+    const num1 = Number(numInput1.value);
+     const num2 = Number(numInput2.value);
     const result =  num1 + num2
-    resultDispaly.textContent = result;
+    resultDispaly.innerHTML = result;
     console.log(result)
 }
 const multiBtn = ()=>{
-   const {num1,num2}=  getNumber()
+     const num1 = Number(numInput1.value);
+     const num2 = Number(numInput2.value);
     const result =  num1 * num2
-    resultDispaly.textContent = result;
+    resultDispaly.innerHTML = result;
 }
  const subsBtn = ()=>{
-   const {num1,num2}=  getNumber()
+   const num1 = Number(numInput1.value);
+     const num2 = Number(numInput2.value);
     const result =  num1 - num2
-    resultDispaly.textContent = result;
+    resultDispaly.innerHTML = result;
 }
- const divBtn = ()=>{
-   const {num1,num2}=  getNumber()
 
+ const divBtn = ()=>{
+     const num1 = Number(numInput1.value);
+     const num2 = Number(numInput2.value);
    if(num2 === 0){
-    resultDispaly.textContent = " this division is error"
+    resultDispaly.innerHTML = " this division is error"
    }
     else{
     const result =  num1 / num2
